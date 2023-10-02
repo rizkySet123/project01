cf68bfe599ae80b73a20798e2aa521aa50111087
rizkySet123 <riskisetiawan88779@gmail.com>
2023-10-02 11:54:37 +0700

    first commit

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 from datetime import datetime
 
-# connection_string = 'mongodb+srv://test:sparta@cluster0.rlckixy.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient('mongodb+srv://test:sparta@cluster0.rlckixy.mongodb.net/?retryWrites=true&w=majority')
+# connection_string = 'mongodb+srv://rizky:rizky123setiawan@cluster0.ed4ndng.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient('mongodb+srv://rizky:rizky123setiawan@cluster0.ed4ndng.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
 app = Flask(__name__)
